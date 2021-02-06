ae745a0e471828cce2803393a46a8fe1badd7cdd
christian <chris_villarruel@outlook.com>
2021-02-05 21:47:32 -0600

    Se creo el controlador que permitira crear un usuario

--- users/api.py
+++ users/api.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+# users.api
 
-# Create your views here.
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .serializers import RegistrationSerializer
+from .detail_error import msg_error
+
+
+class RegistrationAPIView(APIView):
+    # Allow any user (authenticated or not) to hit this endpoint.
+    permission_classes = (AllowAny,)
+    serializer_class = RegistrationSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        # Validaciones
+        if serializer.is_valid():
+            instance = serializer.save()
+            msg_success = {
+                'success': f'Hola {instance.username}',
+                'detal': serializer.data
+            }
+            return Response(msg_success, status=status.HTTP_201_CREATED)
+
+        error = msg_error('Error de validación', 'BAD_REQUEST', 400, serializer.errors)
+        return Response(error, status=status.HTTP_400_BAD_REQUEST)
