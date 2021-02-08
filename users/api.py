0054fc3245a68131daba30480218726e91d0956a
christian <chris_villarruel@outlook.com>
2021-02-07 21:28:54 -0600

    Se creo el controlador para cada serializador

--- users/api.py
+++ users/api.py
@@ -5,12 +5,13 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .serializers import RegistrationSerializer
+from .serializers import RegistrationSerializer, LoginSerializer
 from .detail_error import msg_error
+from .renderers import UserJSONRenderer
 
 
 class RegistrationAPIView(APIView):
-    # Allow any user (authenticated or not) to hit this endpoint.
+    # Permita que cualquier usuario (autenticado o no) llegue a este endpoint.
     permission_classes = (AllowAny,)
     serializer_class = RegistrationSerializer
 
@@ -25,6 +26,25 @@ class RegistrationAPIView(APIView):
                 'detal': serializer.data
             }
             return Response(msg_success, status=status.HTTP_201_CREATED)
+
+        error = msg_error('Error de validación', 'BAD_REQUEST', 400, serializer.errors)
+        return Response(error, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginAPIView(APIView):
+    permission_classes = (AllowAny,)
+    serializer_class = LoginSerializer
+
+    def post(self, request):
+        user = request.data
+
+        # Notice here that we do not call `serializer.save()` like we did for
+        # the registration endpoint. This is because we don't  have
+        # anything to save. Instead, the `validate` method on our serializer
+        # handles everything we need.
+        serializer = self.serializer_class(data=user)
+        if serializer.is_valid():
+            return Response(serializer.data, status=status.HTTP_200_OK)
 
         error = msg_error('Error de validación', 'BAD_REQUEST', 400, serializer.errors)
         return Response(error, status=status.HTTP_400_BAD_REQUEST)
