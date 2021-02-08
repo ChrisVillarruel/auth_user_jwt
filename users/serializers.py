1972fb6dc1a8220ca9807082acf23f00e9e61c85
christian <chris_villarruel@outlook.com>
2021-02-07 21:28:29 -0600

    Se crearon los seriliazadores para register y login

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,6 @@
 # users.serializers
 from rest_framework import serializers
+from django.contrib.auth import authenticate
 
 from .models import User
 
@@ -55,3 +56,47 @@ class RegistrationSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
+
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.CharField(max_length=255)
+    username = serializers.CharField(max_length=255, read_only=True)
+    password = serializers.CharField(max_length=128, min_length=8, write_only=True)
+    token = serializers.CharField(max_length=255, read_only=True)
+
+    def validate(self, data):
+        # El método `validate` es donde nos aseguramos de que la
+        # instancia de `LoginSerializer` tiene" válido ". En el caso de registrar un
+        # usuario en, esto significa validar que ha proporcionado un correo electrónico
+        # y contraseña y que esta combinación coincide con uno de los usuarios en
+        # nuestra base de datos.
+        email = data.get('email', None)
+        password = data.get('password', None)
+
+        # El método `authenticate` es proporcionado por Django y maneja la verificación
+        # para un usuario que coincide con esta combinación de correo electrónico y contraseña. Date cuenta cómo
+        # pasamos `email` como el valor de` username` ya que en nuestro User
+        # modelo establecemos `USERNAME_FIELD` como` email`.
+        user = authenticate(username=email, password=password)
+
+        # Si no se encontró ningún usuario que coincida con esta combinación de correo electrónico / contraseña,
+        # `authenticate` devolverá` None`. Plantee una excepción en este caso.
+        if user is None:
+            raise serializers.ValidationError(
+                'No se encontró un usuario con este correo electrónico y contraseña.')
+
+        # Django proporciona una bandera en nuestro modelo `Usuario` llamada` is_active`. los
+        # El propósito de esta bandera es decirnos si el usuario ha sido baneado.
+        # o desactivado. Este casi nunca será el caso, pero
+        # vale la pena comprobarlo. Plantee una excepción en este caso.
+        if not user.is_active:
+            raise serializers.ValidationError(
+                'Este usuario ha sido desactivado.')
+
+        # El método `validate` debería devolver un diccionario de datos validados.
+        # Estos son los datos que se pasan a los métodos `create` y` update`.
+        return {
+            'email': user.email,
+            'username': user.username,
+            'token': user.token
+        }
