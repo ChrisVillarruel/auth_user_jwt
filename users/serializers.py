234ddef8bf38fb9dfea07ca55cc464ad64c12a1d
christian <chris_villarruel@outlook.com>
2021-02-09 21:38:32 -0600

    Se creo un serializador que me permite crear una lista de usuarios personalizada

--- users/serializers.py
+++ users/serializers.py
@@ -150,3 +150,17 @@ class UserSerializer(serializers.ModelSerializer):
         instance.save()
 
         return instance
+
+
+class UserListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['user_id', 'username', 'email', 'is_active']
+
+    def to_representation(self, instance):
+        return {
+            'id': instance['user_id'],
+            'username': instance['username'],
+            'email': instance['email'],
+            'is_active': instance['is_active']
+        }
