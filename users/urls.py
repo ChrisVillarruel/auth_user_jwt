24de8e075d12151bc2bd219fe629cf8e2c27831b
christian <chris_villarruel@outlook.com>
2021-02-15 15:04:29 -0600

    Todas la rutas funcionan con exito

--- users/urls.py
+++ users/urls.py
@@ -4,6 +4,7 @@ from .api import *
 urlpatterns = [
     path('register/', RegistrationAPIView.as_view()),
     path('login/', LoginAPIView.as_view()),
-    path('user', UserRetrieveUpdateAPIView.as_view()),
+    path('user/', UserRetrieveUpdateAPIView.as_view()),
     path('users/', UsersList.as_view()),
+    # path('logout/', LogoutAPIView.as_view()),
 ]
