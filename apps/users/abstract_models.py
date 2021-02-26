6dba5834ef6fb489cafccd0a888e2e1884252292
christian <chris_villarruel@outlook.com>
2021-02-25 18:21:44 -0600

    Se movieron a nuevo modulo las clases abstractas de la entidad User

---
+++ apps/users/abstract_models.py
@@ -0,0 +1,24 @@
+# Modulo de django
+from django.db import models
+
+
+class UserToken(models.Model):
+    # access Token
+    access_token = models.CharField(db_index=True, max_length=255, null=True, default=None)
+
+    # refresh_token
+    refresh_token = models.CharField(db_index=True, max_length=255, null=True, default=None)
+
+    class Meta:
+        abstract = True
+
+
+class CreateOrUpdateUser(models.Model):
+    # Una marca de tiempo que representa cuándo se creó este objeto.
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    # Una marca de tiempo que representa cuándo se actualizo este objeto.
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
