69d3783577b9bc1ccf8c1dcdbd14769d6fde93f8
christian <chris_villarruel@outlook.com>
2021-02-25 19:58:08 -0600

    Se rearlizaron algunos cambios

--- apps/authentication/timezone.py
+++ apps/authentication/timezone.py
@@ -7,10 +7,19 @@ import pytz
 
 
 def get_timezone():
-    # date_now = datetime.datetime.now(tz=pytz.timezone('America/Mexico_City'))
+    """
+    Obtenemos la hora local
+
+    """
     return datetime.datetime.now(tz=pytz.timezone('America/Mexico_City'))
 
 
 def get_timedelta(days=0, minutes=0):
-    exp = get_timezone() + datetime.timedelta(days=days, minutes=minutes)
-    return exp
+    """
+    De la hora local obtenida, definimos los dias o minutos que van
+    a suceder en el futuro
+
+    """
+
+
+    return get_timezone() + datetime.timedelta(days=days, minutes=minutes)
