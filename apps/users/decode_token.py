8c9cc0342258a21ede95d792e7b2872961035a66
christian <chris_villarruel@outlook.com>
2021-02-25 18:19:59 -0600

    Se realizaron algunos comentarios dentro del codigo

--- apps/users/decode_token.py
+++ apps/users/decode_token.py
@@ -1,21 +1,35 @@
-# users.decode_token
 
+# Modulos de rest_framework
 from rest_framework import exceptions
-from datetime import datetime, timedelta
+
+# Modulos de Django
 from django.conf import settings
+
+# Modulos de Python
+from datetime import datetime, timedelta
+
+# Modulos de jwt
 import jwt
 
 
-# Creamos el metodo get_date que me retornara la fecha de expiración del token actual
 def get_token_expiration_date(token):
+    """
+    Creamos el metodo get_date que me retornara la fecha de expiración del token actual
+
+    """
 
     try:
         payload = jwt.decode(token, settings.SECRET_KEY)
-        # De la fecha de expiración del token le restamos
-        # cinco dias, para que se actualice el token antes
-        # de la fecha de expiración.
+        """
+        De la fecha de expiración del token le restamos
+        cinco dias, para que se actualice el token antes
+        de la fecha de expiración.
+
+        """
+
         timestamp = payload['exp'] - 500000
         dt_object = datetime.fromtimestamp(timestamp)
+
         return dt_object.strftime('%y%m%d')
 
     except jwt.ExpiredSignatureError as e:
