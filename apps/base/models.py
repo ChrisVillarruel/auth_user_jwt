28226e9f64d28014a9c017a651f4e788d43790b7
christian <chris_villarruel@outlook.com>
2021-03-09 14:25:53 -0600

    Se creron modelos abstractos

--- apps/base/models.py
+++ apps/base/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class BaseModel(models.Model):
+
+    # Una marca de tiempo que representa cuándo se creó este objeto.
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    # Una marca de tiempo que representa cuándo se actualizo este objeto.
+    updated_at = models.DateTimeField(auto_now=True)
+
+    """
+    Se definio un nuevo campo, que permitira dar de baja temporal una cuenta
+    sin necesidad de eliminar la cuenta completamente
+
+    """
+    state = models.BooleanField(default=True)
+
+    class Meta:
+        abstract = True
+
+
+class UserBaseToken(models.Model):
+    # access Token
+    access_token = models.CharField(db_index=True, max_length=500, null=True, default=None)
+
+    # refresh_token
+    refresh_token = models.CharField(db_index=True, max_length=500, null=True, default=None)
+
+    class Meta:
+        abstract = True
