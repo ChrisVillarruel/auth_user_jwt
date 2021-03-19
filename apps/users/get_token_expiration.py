9e86880591484c7650483c178b82ba2c60fa54d1
ChrisVillarruel <givc980909@gmail.com>
2021-03-19 14:39:30 -0600

    Se realizaron cambios en los mensajes de retorno cuando hay una excepción

---
+++ apps/users/get_token_expiration.py
@@ -0,0 +1,40 @@
+
+# Modulos de rest_framework
+from rest_framework import exceptions
+
+# Modulos de Django
+from django.conf import settings
+
+# Modulos de Python
+from datetime import datetime, timedelta
+
+# Modulos de jwt
+import jwt
+
+
+def get_token_expiration_date(token):
+    """
+    Creamos el metodo get_token_expiration_date que me retornara la fecha de expiración del token actual
+
+    """
+
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY)
+        """
+        De la fecha de expiración del token le restamos
+        cinco dias, para que se actualice el token antes
+        de la fecha de expiración.
+
+
+        """
+        timestamp = payload['exp'] - 500000
+        dt_object = datetime.fromtimestamp(timestamp)
+        return dt_object.strftime('%y%m%d')
+
+    except jwt.ExpiredSignatureError as e:
+        msg = 'Sesión expirada vuelva a iniciar sesión para continuar.'
+        raise exceptions.AuthenticationFailed(msg)
+
+    except jwt.exceptions.DecodeError as e:
+        msg = 'Autenticación no válida. No se pudo decodificar el token.'
+        raise exceptions.AuthenticationFailed(msg)
