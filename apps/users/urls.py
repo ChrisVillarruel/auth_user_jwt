83585a19abf779e553c168465aa8c7898929edfe
ChrisVillarruel <givc980909@gmail.com>
2021-03-19 14:27:11 -0600

    Se modifico la ruta para cerrar sesión, ahora es una vista generica

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -2,9 +2,9 @@ from django.urls import path, include
 from apps.users.api.views.user_views import *
 
 urlpatterns = [
-    path('create-account/', UserCreateAPIView.as_view()),
     path('login/', LoginAPIView.as_view()),
-    path('logout/', LogoutAPIView.as_view()),
+    path('create-account/', UserCreateAPIView.as_view()),
+    path('logout/', LogoutRetrieveDestroyAPIView.as_view()),
     path('account/', UserRetrieveUpdateDestroyAPIView.as_view()),
     path('account-delete/', UserRetrieveDestroyAPIView.as_view()),
 ]
