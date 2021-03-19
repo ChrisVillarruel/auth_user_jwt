6bad1e6b1c3f9041af8030af09ed991f766db29b
ChrisVillarruel <givc980909@gmail.com>
2021-03-19 14:32:00 -0600

    Se añadieron nuevos metodos

--- detail_error.py
+++ detail_error.py
@@ -1,15 +1,33 @@
 from datetime import datetime
 
 
-def msg_error(message, status, code, detail=None):
-    if detail is None:
-        detail = 'Error no detallado'
-
-    msg_error = {
-        'message': message,
-        'status': status,
-        'code': code,
-        'date': datetime.now().strftime("%d-%m-%Y"),
-        'diagnosticMessages': detail
+def base_resource(message, code):
+    return {
+        'detail': message,
+        'status_code': code
     }
-    return msg_error
+
+
+def resource_created():
+    created = base_resource(message='Recurso Creado.', code=201)
+    return created
+
+
+def resource_updated():
+    update = base_resource(message='Usuario Actualizado.', code=200)
+    return update
+
+
+def resource_destroy():
+    destroy = base_resource(message='Cuenta Eliminada.', code=200)
+    return destroy
+
+
+def not_found():
+    not_found = base_resource(message='No encontrado.', code=404)
+    return not_found
+
+
+def logout():
+    logout = base_resource(message='Sesión Finalizada', code=204)
+    return logout
